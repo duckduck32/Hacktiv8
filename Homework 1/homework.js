23f7b4eb9a0c76f5943ad137740ff88e3737f4e7
duckduck32 <leonjonathanfarrell@gmail.com>
2024-06-01 15:43:53 +0700

    Commit Homework 1

---
+++ Homework 1/homework.js
@@ -0,0 +1,65 @@
+const videoData = [
+  {
+    name: "Miss Scarlet",
+    present: true,
+    rooms: {
+      kitchen: false,
+      ballroom: false,
+      conservatory: false,
+      "dining room": false,
+      "billiard room": false,
+      library: false,
+    },
+  },
+  {
+    name: "Rusty",
+    present: false,
+    rooms: {
+      kitchen: false,
+      ballroom: false,
+      conservatory: false,
+      "dining room": false,
+      "billiard room": false,
+      library: false,
+    },
+  },
+  {
+    name: "Colonel Mustard",
+    present: true,
+    rooms: {
+      kitchen: false,
+      ballroom: false,
+      conservatory: false,
+      "dining room": false,
+      "billiard room": false,
+      library: false,
+    },
+  },
+  {
+    name: "Professor Plum",
+    present: true,
+    rooms: {
+      kitchen: false,
+      ballroom: false,
+      conservatory: false,
+      "dining room": false,
+      "billiard room": false,
+      library: false,
+    },
+  },
+];
+
+let arrayFilter = videoData.filter(function (el) {
+  return el.present == true;
+});
+
+let trueCount = 0;
+
+for (present in arrayFilter) {
+  trueCount += 1;
+}
+
+console.log("Jumlah Present True ada " + trueCount);
+console.log(arrayFilter);
+
+//Assignment 1 Sesi 2 Leon Jonathan Farrell
